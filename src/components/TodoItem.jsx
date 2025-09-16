4737700e3f7ea2d29a9d32dfe2be9b2fc7b433f6
natia <natia.karamnishvili369@law.tsu.ge>
2025-09-16 17:11:34 +0400

    feat: add reducer functions (add, prepare, delete)

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {useSelector} from 'react-redux' 
 
 function TodoItem() {
+  const todo = useSelector((state) => state.todos)
   return (
     <div>TodoItem</div>
   )
 }
 
-export default TodoItem
+export default TodoItem 
--- src/components/TodoSlice.js
+++ src/components/TodoSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
     todos: [],
@@ -9,10 +9,24 @@ const todoSlice = createSlice(
         name: 'todos',
         initialState,
         reducers:{
+            addTodo:{
+                reducer(state, action){
+                    state.todos.push(action.payload);
+                },
+                prepare(text){
+                    return {payload: {id:nanoid(), text}}
+                }
+                ,
+                deleteTodo(state, action){
+                    state.todos = state.todos.filter((td) => td.id !== action.payload)
+                }      
+            }
+                     
             
         }
     }
 )
 
+export const {addTodo, deleteTodo } = todoSlice.actions
 export default todoSlice.reducer;
 
