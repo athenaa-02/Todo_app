6e0d4693077474bfd5efe409f7c2fd2547be03d0
natia <natia.karamnishvili369@law.tsu.ge>
2025-09-17 00:42:37 +0400

    feat: todoitem styling

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,14 +6,16 @@ function TodoItem({ todo }) {
   const dispatch = useDispatch();
 
   return (
-    <div className="parent">
+    <div className="todo_parent">
       <div className="text_wrapper">
       <div className="todo_name">{todo.text}</div>
       <span>time</span>
       </div>
       <div className="btn_wrapper">
-      <input type="checkbox" />
-      <button onClick={() => dispatch(deleteTodo(todo.id))}>delete</button>
+      <input type="checkbox" className="checkbox" />
+      <button className="delete" onClick={() => dispatch(deleteTodo(todo.id))}>
+        <img src="/public/delete.png" alt="" />
+      </button>
     </div>
     </div>
   );
