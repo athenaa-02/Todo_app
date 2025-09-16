e73f1fc4bc260c18b7d67f5b99d16fa555866bb7
natia <natia.karamnishvili369@law.tsu.ge>
2025-09-16 18:22:35 +0400

    feat: add form and input elements, with submit handler and useState hook(for input text)

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "./TodoSlice.js";
 
 function TodoInput() {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() === "") return;
+    dispatch(addTodo(text));
+    setText("");
+  };
+
   return (
-    <div>TodoInput</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Note"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit"></button>
+    </form>
+  );
 }
 
-export default TodoInput
+export default TodoInput;
