4232dce0e84a7e91165660249d6af7f2d84d0199
natia <natia.karamnishvili369@law.tsu.ge>
2025-09-17 11:41:36 +0400

    fix: delete Todo function error

--- src/components/TodoSlice.js
+++ src/components/TodoSlice.js
@@ -1,32 +1,26 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [],
+  todos: [],
 };
 
-const todoSlice = createSlice(
-    {
-        name: 'todos',
-        initialState,
-        reducers:{
-            addTodo:{
-                reducer(state, action){
-                    state.todos.push(action.payload);
-                },
-                prepare(text){
-                    return {payload: {id:nanoid(), text}}
-                }
-                ,
-                deleteTodo(state, action){
-                    state.todos = state.todos.filter((td) => td.id !== action.payload)
-                }      
-            }
-                     
-            
-        }
-    }
-)
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: {
+      reducer(state, action) {
+        state.todos.push(action.payload);
+      },
+      prepare(text) {
+        return { payload: { id: nanoid(), text } };
+      },
+    },
+    deleteTodo(state, action) {
+      state.todos = state.todos.filter((td) => td.id !== action.payload);
+    },
+  },
+});
 
-export const {addTodo, deleteTodo } = todoSlice.actions
+export const { addTodo, deleteTodo } = todoSlice.actions;
 export default todoSlice.reducer;
-
